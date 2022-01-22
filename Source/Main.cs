d5ee42487fbf1aa363c6d0161c2b5dd032a7ec36
Doug Benham <dougbenham@cox.net>
2022-01-21 20:41:10 -0700

    Add project files.

---
+++ Source/Main.cs
@@ -0,0 +1,90 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace AutomaticScheduler
+{
+    [StaticConstructorOnStartup]
+    public static class AutomaticScheduler
+    {
+        private static void AutoSchedule(Pawn pawn)
+        {
+            if (pawn?.timetable != null && !WorldComp.PawnsWithScheduleAdjusted.Contains(pawn))
+            {
+                if (pawn.story?.traits?.HasTrait(TraitDefOf.NightOwl) == true)
+                {
+                    pawn.timetable.times = new List<TimeAssignmentDef>(GenDate.HoursPerDay);
+                    for (int i = 0; i < GenDate.HoursPerDay; i++)
+                    {
+                        pawn.timetable.times.Add(i >= 11 && i <= 18 ? TimeAssignmentDefOf.Sleep : TimeAssignmentDefOf.Anything);
+                    }
+                }
+                else
+                {                    
+                    pawn.timetable.times = new List<TimeAssignmentDef>(GenDate.HoursPerDay);
+                    for (int i = 0; i < GenDate.HoursPerDay; i++)
+                    {
+                        TimeAssignmentDef setNightOwlHours = ((i >= 20 && i <= 23) || (i >= 4 && i <= 7)) ? TimeAssignmentDefOf.Sleep : TimeAssignmentDefOf.Anything;
+                        pawn.timetable.times.Add(setNightOwlHours);
+                    }
+                }
+
+                WorldComp.PawnsWithScheduleAdjusted.Add(pawn);
+            }
+        }
+
+        [HarmonyPatch(typeof(Thing), nameof(Thing.SpawnSetup))]
+        public static class Patch_Thing_SpawnSetup
+        {
+            // Patching for initial pawns
+            public static void Postfix(Thing __instance)
+            {
+                if (__instance is Pawn p && p.Faction?.IsPlayer == true && p.def?.race?.Humanlike == true)
+                {
+                    AutoSchedule(p);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(InteractionWorker_RecruitAttempt), nameof(InteractionWorker_RecruitAttempt.DoRecruit), new Type[] { typeof(Pawn), typeof(Pawn), typeof(string), typeof(string), typeof(bool), typeof(bool) }, new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out, ArgumentType.Normal, ArgumentType.Normal })]
+        public static class Patch_InteractionWorker_RecruitAttempt
+        {
+            // Patching for recruited prisoners
+            public static void Postfix(Pawn recruiter, Pawn recruitee)
+            {
+                if (recruitee is Pawn p && p.Faction?.IsPlayer == true && p.def?.race?.Humanlike == true)
+                {
+                    AutoSchedule(p);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(InteractionWorker_EnslaveAttempt), nameof(InteractionWorker_EnslaveAttempt.Interacted))]
+         public static class Patch_InteractionWorker_EnslaveAttempt
+         { 
+             // Patching for enslaved prisoners
+             public static void Postfix(Pawn initiator, Pawn recipient)
+             {
+                 if (recipient is Pawn p && p.GuestStatus == GuestStatus.Slave)
+                 {
+                     AutoSchedule(p);
+                 }
+             }
+         }
+
+        static AutomaticScheduler()
+        {
+            var harmony = new Harmony("doug.AutomaticScheduler");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [DefOf]
+        public static class TraitDefOf
+        {
+            public static TraitDef NightOwl;
+        }
+    }
+}
---
+++ Source/WorldComp.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using RimWorld.Planet;
+using Verse;
+
+namespace AutomaticScheduler
+{
+    class WorldComp : WorldComponent
+    {
+        // Using a HashSet for quick lookup
+        public static HashSet<Pawn> PawnsWithScheduleAdjusted = new HashSet<Pawn>();
+        // I've found it easier to have a null list for use when exposing data
+        // This shouldn't be needed but mods will remove Pawns from the game completely (RuntimeGC for instance)
+        // and HashSet will fail if more than one null value is added.
+        private List<Pawn> usedForExposingData = null;
+
+        public WorldComp(World w) : base(w)
+        {
+            // Make sure the static HashSet is cleared whenever a game is created or loaded.
+            PawnsWithScheduleAdjusted.Clear();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            if (Scribe.mode == LoadSaveMode.Saving)
+            {
+                // When saving, populate the list
+                usedForExposingData = new List<Pawn>(PawnsWithScheduleAdjusted);
+            }
+
+            Scribe_Collections.Look(ref usedForExposingData, "pawnsWithScheduleAdjusted", LookMode.Reference);
+
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                // When loading, clear the HashSet then populate it with the loaded data
+                PawnsWithScheduleAdjusted.Clear();
+                foreach (var v in usedForExposingData)
+                {
+                    // Remove any null records
+                    if (v != null)
+                    {
+                        PawnsWithScheduleAdjusted.Add(v);
+                    }
+                }
+            }
+
+            if (Scribe.mode == LoadSaveMode.Saving ||
+                Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                // Add hints to the garbage collector that this memory can be collected
+                usedForExposingData?.Clear();
+                usedForExposingData = null;
+            }
+        }
+    }
+}
